e20bd58516eee6e8cccf02222b47312e223cf2f9
AlessandroColi <AlessandroColi@users.noreply.github.com>
2024-06-01 13:28:41 +0000

    Deploying to gh-pages from @ AlessandroColi/pulvreakt@83cd308833b00b7e349a73adde14ee6af92e43aa 🚀

---
+++ index.search.js
@@ -0,0 +1,94 @@
+var relearn_search_index = [
+  {
+    "breadcrumb": "Pulverization framework",
+    "content": "Contents ",
+    "description": "TODO",
+    "tags": [],
+    "title": "Tutorials",
+    "uri": "/tutorials/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework",
+    "content": "Contents ",
+    "description": "TODO",
+    "tags": [],
+    "title": "How-to Guides",
+    "uri": "/how-to-guides/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework \u003e Reference",
+    "content": "",
+    "description": "KDoc API docs",
+    "tags": [
+      "Kdoc",
+      "Dokka",
+      "Api Docs"
+    ],
+    "title": "KDoc API",
+    "uri": "/reference/kdoc/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework",
+    "content": "Contents ",
+    "description": "TODO",
+    "tags": [],
+    "title": "Explanation",
+    "uri": "/explanation/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework",
+    "content": "Contents KDoc APIKDoc API docs",
+    "description": "TODO",
+    "tags": [],
+    "title": "Reference",
+    "uri": "/reference/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Api Docs",
+    "uri": "/tags/api-docs/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Dokka",
+    "uri": "/tags/dokka/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework \u003e Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: Kdoc",
+    "uri": "/tags/kdoc/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Pulverization framework",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Pulverization framework",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
